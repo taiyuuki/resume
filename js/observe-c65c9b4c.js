0e1bc09ec38448b380893901be4c574a7d4673d3
taiyuuki <taiyuuki@qq.com>
2022-07-15 19:47:54 +0800

    build

---
+++ js/observe-c65c9b4c.js
@@ -0,0 +1,1 @@
+import{b as r}from"./index-6caaaaf2.js";var t=r(async({app:o})=>{o.config.globalProperties.$observer={handler(e){e.isIntersecting?e.target.classList.add("active"):e.target.classList.remove("active")},cfg:{root:null,rootMargin:"-65px 0px 0px 0px",threshold:0}}});export{t as default};
